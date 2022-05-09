858336cdba9d3f5803e2e5dc82b4ca38417475be
AlbertoSardo <sardoalbi@gmail.com>
2022-05-09 18:38:16 +0200

    Tests Implemented for Table Class

--- src/model/TableTest.java
+++ src/model/TableTest.java
@@ -1,5 +1,7 @@
-package src.model;
+package model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -7,9 +9,27 @@ import org.junit.Test;
 /**
  * The test class TableTest.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Nicolo' Tafta & Alberto Sardo
+ * @version 9/05/2022
  */
 public class TableTest {
-    //
+
+    static private final int sizeTable = 40;
+    Square[] cell = new Square[sizeTable];
+
+    @Test
+    public void newtabletest(){
+        Table table = new Table();
+    }
+
+    @Test
+    public void getSquareTest(){
+        Table table = new Table();
+        assertTrue(new PropertySquare(400, 50, "Piazza della Riforma", "blue").getName().equals(table.getSquare(39).getName()));
+    }
+
+    @Test 
+    public void getDescriptionPropertyTest(){
+        assertEquals(expected,  getDescriptionProperty(39));
+    }
 }
